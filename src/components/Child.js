b63b3a429d759f7c53cdba0cd871061a7661cc08
Arjun Singh <arjunsinghbazal@gmail.com>
2023-06-13 13:05:03 +0530

    submitting code-Tue Jun 13 2023 13:05:02 GMT+0530 (India Standard Time)

--- src/components/Child.js
+++ src/components/Child.js
@@ -7,10 +7,10 @@ return(
     <h1>Child Component</h1>
     <button onClick={setModal}>Show Model</button>
     {showModal&&(
-        <h3>
+        <div className="child">
           <h3>Modal Content</h3>
           <p>This is the modal content.</p>
-        </h3>
+        </div>
       )}
 </>)
 }
